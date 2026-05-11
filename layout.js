36c9efd9c0ee7b3bf28efa1c35b182f58abd82ec
subversieve <121326740+subversieve@users.noreply.github.com>
2026-05-10 19:10:42 -0600

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,13 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+  <defs>
+    <linearGradient id="g" x1="0%" y1="0%" x2="100%" y2="0%">
+      <stop offset="0%" stop-color="hsl(60,95%,55%)"/>
+      <stop offset="25%" stop-color="hsl(35,95%,55%)"/>
+      <stop offset="55%" stop-color="hsl(12,95%,55%)"/>
+      <stop offset="80%" stop-color="hsl(350,95%,55%)"/>
+      <stop offset="100%" stop-color="hsl(320,95%,55%)"/>
+    </linearGradient>
+  </defs>
+  <path d="M4 16 C 6 6, 10 26, 12 16 S 18 6, 20 16 S 26 26, 27 16 a 3 3 0 1 1 0 0.1"
+        fill="none" stroke="url(#g)" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
